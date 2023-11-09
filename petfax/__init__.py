92155607a4997cb9525cecf6c88cbb9e95bc7c18
Jose R  Torres <joker@joses-mbp.lan>
2023-11-08 21:48:28 -0800

    finished creating views

---
+++ petfax/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+
+# factory
+def create_app():
+    [ ... ]
+
+    # register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    # return the app 
+    return app
+
---
+++ petfax/pet.py
@@ -0,0 +1,8 @@
+from flask import (Blueprint , render_template)
+bp = Blueprint('pet', __name__, url_prefix="/pets")
+import json
+pets = json.loads(open('pets.json'))
+print(pets)
+@bp.route('/')
+def index(): 
+    return render_template('index.html', pets=pets)
